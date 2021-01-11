262d7f8843578539a385fda492c80efc823670eb
Rodrigo <r.ramosesteves@hotmail.com>
2021-01-11 12:51:06 +0000

    Adicionado .gitignore Modificado main

--- src/pt/serro/test/Main.java
+++ src/pt/serro/test/Main.java
@@ -3,6 +3,6 @@ package pt.serro.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-	System.out.println("Asais Wrld!")
+	    System.out.println("Asais Wrld com queijo!");
     }
 }
